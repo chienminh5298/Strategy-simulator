61224cda87a947f3f4b375e711b66b106705773a
Minh C Nguyen <chienminh5298@gmail.com>
2025-04-22 10:56:38 -0400

    reset config

--- src/redux/configReducer.tsx
+++ src/redux/configReducer.tsx
@@ -37,11 +37,11 @@ const initialState: {
             stoplosses: [
                 {
                     target: 0,
-                    percent: -1.85,
+                    percent: -0.7,
                 },
                 {
-                    target: 0.6,
-                    percent: 0.6,
+                    target: 0.7,
+                    percent: 0.7,
                 },
             ],
         },
@@ -50,11 +50,11 @@ const initialState: {
             stoplosses: [
                 {
                     target: 0,
-                    percent: -1.5,
+                    percent: -0.7,
                 },
                 {
-                    target: 4.2,
-                    percent: 4.2,
+                    target: 0.7,
+                    percent: 0.7,
                 },
             ],
         },
